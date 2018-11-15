f30152a5c7ecade7d3eb09e3ff0df07f06c918cf
amithadiraju <amith@kokomo247.com>
2018-11-15 14:50:50 -0500

    Changed some cooments

--- application.py
+++ application.py
@@ -19,7 +19,10 @@ api = Api(app)
 
 #Initializing cross origin callable app
 CORS(app)
+
 #Changed this thing here
+
+# Also changed this
 class Firebase_Data(Resource):
 
     '''This class is used to connect to a firestore database and fetch the data from it.
